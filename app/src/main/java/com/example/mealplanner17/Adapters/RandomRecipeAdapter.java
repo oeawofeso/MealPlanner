e998b52d0832bfa55e62e3b581ffcf5abef53c4e
JLohmanALB <89558746+JLohmanALB@users.noreply.github.com>
2024-04-18 15:31:51 -0400

    Sides

--- app/src/main/java/com/example/mealplanner17/Adapters/RandomRecipeAdapter.java
+++ app/src/main/java/com/example/mealplanner17/Adapters/RandomRecipeAdapter.java
@@ -1,14 +1,13 @@
 package com.example.mealplanner17.Adapters;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,63 +19,64 @@ import com.example.mealplanner17.ModelsAPI.Recipe;
 import com.example.mealplanner17.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHolder> {
+public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeAdapter.RandomRecipeViewHolder> {
+    private Context context;
+    private List<Recipe> recipeList;
+    private RecipeClickListener listener;
 
-    Context context;
-    List<Recipe> list;
-    RecipeClickListener listener;
-
-    public RandomRecipeAdapter(Context context, List<Recipe> list, RecipeClickListener listener) {
+    // Constructor for the adapter
+    public RandomRecipeAdapter(Context context, List<Recipe> recipeList, RecipeClickListener listener) {
         this.context = context;
-        this.list = list;
+        this.recipeList = recipeList;
         this.listener = listener;
     }
 
     @NonNull
     @Override
     public RandomRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new RandomRecipeViewHolder(LayoutInflater.from(context).inflate(R.layout.list_random_recipe, parent, false));
+        View view = LayoutInflater.from(context).inflate(R.layout.list_random_recipe, parent, false);
+        return new RandomRecipeViewHolder(view);
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull RandomRecipeViewHolder holder, int position) {
-        Recipe recipe = list.get(position);
+        Recipe recipe = recipeList.get(position);
 
-        holder.textView_title.setText(recipe.title);
-        holder.textView_title.setSelected(true);
-        holder.TextView_like.setText(recipe.aggregateLikes + " Likes");
-        holder.TextView_servings.setText(recipe.servings + " Servings");
+        holder.textViewTitle.setText(recipe.title);
+        holder.textViewTitle.setSelected(true);
+        holder.textViewLikes.setText(recipe.aggregateLikes + " Likes");
+        holder.textViewServings.setText(recipe.servings + " Servings");
 
-        Picasso.get().load(recipe.image).into(holder.imageView_food);
+        Picasso.get().load(recipe.image).into(holder.imageViewFood);
 
         // Set favorite button status
         SharedPreferences sharedPreferences = context.getSharedPreferences("MyFavoriteMeals", Context.MODE_PRIVATE);
         boolean isFavorite = sharedPreferences.getBoolean(String.valueOf(recipe.id), false);
         holder.btnFavorite.setSelected(isFavorite);
 
-        holder.btnFavorite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleFavoriteStatus(recipe, holder.btnFavorite);
-            }
-        });
+        holder.btnFavorite.setOnClickListener(v -> toggleFavoriteStatus(recipe, holder.btnFavorite));
 
-        holder.random_list_container.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onRecipeClick(String.valueOf(recipe.id));
-            }
-        });
+        holder.randomListContainer.setOnClickListener(v -> listener.onRecipeClick(String.valueOf(recipe.id)));
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return recipeList.size();
     }
 
+    // Method to update the data in the adapter
+    public void updateData(List<Recipe> newRecipes) {
+        // Clear the current list and add the new recipes
+        recipeList.clear();
+        recipeList.addAll(newRecipes);
+        // Notify the adapter that the data has changed
+        notifyDataSetChanged();
+    }
+
+    // Method to toggle favorite status of a recipe
     private void toggleFavoriteStatus(Recipe recipe, Button btnFavorite) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("MyFavoriteMeals", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -92,22 +92,23 @@ public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHo
         editor.apply();
         btnFavorite.setSelected(!btnFavorite.isSelected());
     }
-}
-class RandomRecipeViewHolder extends RecyclerView.ViewHolder{
-   CardView random_list_container;
-   TextView textView_title,TextView_servings,TextView_like;
-   ImageView imageView_food;
-    Button btnFavorite;
 
+    // Inner ViewHolder class
+    public static class RandomRecipeViewHolder extends RecyclerView.ViewHolder {
+        CardView randomListContainer;
+        TextView textViewTitle, textViewServings, textViewLikes;
+        ImageView imageViewFood;
+        Button btnFavorite;
 
-    public RandomRecipeViewHolder(@NonNull View itemView) {
-        super(itemView);
-        random_list_container = itemView.findViewById(R.id.random_list_container);
-        textView_title = itemView.findViewById(R.id.textView_title);
-        TextView_servings = itemView.findViewById(R.id.TextView_servings);
-        TextView_like = itemView.findViewById(R.id.TextView_like);
-        imageView_food = itemView.findViewById(R.id.imageView_food);
-        btnFavorite = itemView.findViewById(R.id.btn_favorite);
+        public RandomRecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            randomListContainer = itemView.findViewById(R.id.random_list_container);
+            textViewTitle = itemView.findViewById(R.id.textView_title);
+            textViewServings = itemView.findViewById(R.id.textView_servings);
+            textViewLikes = itemView.findViewById(R.id.textView_like);
+            imageViewFood = itemView.findViewById(R.id.imageView_food);
+            btnFavorite = itemView.findViewById(R.id.btn_favorite);
+        }
     }
 }
 
