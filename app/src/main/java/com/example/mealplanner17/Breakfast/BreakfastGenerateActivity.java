934883e055415f8a5abbf59174668f87a5c231e6
JLohmanALB <89558746+JLohmanALB@users.noreply.github.com>
2024-04-18 23:38:15 -0400

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 5132f093816b4998104b5d83da5615dca34db0f9, reversing
    changes made to 27a163de535942867d2dea9e4aca3c803f4df458.

--- app/src/main/java/com/example/mealplanner17/Breakfast/BreakfastGenerateActivity.java
+++ app/src/main/java/com/example/mealplanner17/Breakfast/BreakfastGenerateActivity.java
@@ -4,184 +4,162 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mealplanner17.API.RecipeDetailsActivity;
+import com.example.mealplanner17.API.RequestManager;
+import com.example.mealplanner17.Listeners.RecipeClickListener;
+
+
 import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.example.mealplanner17.API.RecipeDetailsActivity;
-import com.example.mealplanner17.API.RequestManager;
 import com.example.mealplanner17.Adapters.RandomRecipeAdapter;
 import com.example.mealplanner17.Listeners.RandomRecipeResponseListener;
-import com.example.mealplanner17.Listeners.RecipeClickListener;
 import com.example.mealplanner17.ModelsAPI.RandomRecipeApiResponse;
-import com.example.mealplanner17.ModelsAPI.Recipe;
 import com.example.mealplanner17.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class BreakfastGenerateActivity extends AppCompatActivity {
-    private ProgressDialog dialog;
-    private RequestManager manager;
-    private RandomRecipeAdapter randomRecipeAdapter;
-    private RecyclerView recyclerView;
-    private Button button;
-    private Spinner spinner;
-    private Spinner spinnerEx;
-    private List<String> breakfastTags = new ArrayList<>();
-    private List<String> breakfastTagsExclude = new ArrayList<>();
-    private Set<String> clickedRecipeIds = new HashSet<>();
-    private SharedPreferences sharedPreferences;
-    private static final String PREF_LAST_CLEAR_TIMESTAMP = "last_clear_timestamp";
-    private static final long CLEAR_INTERVAL = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter randomRecipeAdapter;
+    RecyclerView recyclerView;
 
+    FirebaseAuth auth;
+    Button button;
+    FirebaseUser user;
+
+    //spinner object
+    Spinner spinner;
+    Spinner spinnerEx;
+    List<String> breakfastTags= new ArrayList<>();
+    List<String> breakfastTagsExclude= new ArrayList<>();
+
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast_generate);
-
-        // Initialize SharedPreferences
-        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-
-        // Check if it's time to clear the list
-        checkClearClickedRecipeIds();
-
-        initializeViews();
-        setupSpinners();
-        setupRequestManager();
-
-        randomRecipeAdapter = new RandomRecipeAdapter(BreakfastGenerateActivity.this, new ArrayList<>(), recipeClickListener);
-        recyclerView.setAdapter(randomRecipeAdapter);
-    }
-
-    private void checkClearClickedRecipeIds() {
-        long lastClearTimestamp = sharedPreferences.getLong(PREF_LAST_CLEAR_TIMESTAMP, 0);
-        long currentTimestamp = System.currentTimeMillis();
-
-        // Check if 7 days have passed since the last clear
-        if (currentTimestamp - lastClearTimestamp >= CLEAR_INTERVAL) {
-            clickedRecipeIds.clear(); // Clear the list
-            sharedPreferences.edit().putLong(PREF_LAST_CLEAR_TIMESTAMP, currentTimestamp).apply(); // Save current timestamp as last clear timestamp
-        }
-    }
-
-    private void initializeViews() {
-        dialog = new ProgressDialog(this);
+        dialog=new ProgressDialog(this);
         dialog.setTitle("Loading");
-        spinner = findViewById(R.id.spinner_tags);
-        spinnerEx = findViewById(R.id.spinner_tags2);
-        recyclerView = findViewById(R.id.recycler_random);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-    }
 
-    private void setupSpinners() {
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.spinner_text, getResources().getStringArray(R.array.breakfastTags));
+
+
+        spinner = findViewById(R.id.spinner_tags);
+        ArrayAdapter arrayAdapter= ArrayAdapter.createFromResource(this, R.array.breakfastTags, R.layout.spinner_text);
         arrayAdapter.setDropDownViewResource(R.layout.spinner_inner_text);
         spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(spinnerSelectedListener);
 
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, R.layout.spinner_text, getResources().getStringArray(R.array.breakfastTagsExclude));
+        spinnerEx = findViewById(R.id.spinner_tags2);
+        ArrayAdapter arrayAdapter2= ArrayAdapter.createFromResource(this, R.array.breakfastTagsExclude, R.layout.spinner_text);
         arrayAdapter2.setDropDownViewResource(R.layout.spinner_inner_text);
         spinnerEx.setAdapter(arrayAdapter2);
         spinnerEx.setOnItemSelectedListener(spinnerSelectedListener2);
-    }
 
-    private void setupRequestManager() {
-        manager = new RequestManager(this);
+
+        manager=new RequestManager(this);
+        breakfastTags.clear();
         breakfastTags.add("breakfast");
+
+        breakfastTagsExclude.clear();
         breakfastTagsExclude.add("breakfast");
-        manager.getRandomRecipes(randomRecipeResponseListener, breakfastTags, breakfastTagsExclude);
-        dialog.show();
+         manager.getRandomRecipes(randomRecipeResponseListener, breakfastTags,breakfastTagsExclude);
+          dialog.show();
+
+
+
+
+
+
     }
 
-    private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
-        @SuppressLint("NotifyDataSetChanged")
+    private final RandomRecipeResponseListener randomRecipeResponseListener=new RandomRecipeResponseListener() {
         @Override
         public void didFetch(RandomRecipeApiResponse response, String message) {
             dialog.dismiss();
+            recyclerView = findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(BreakfastGenerateActivity.this,1));
 
-            // Filter out the recipes that have been clicked
-            List<Recipe> filteredRecipes = response.recipes.stream()
-                    .filter(recipe -> !clickedRecipeIds.contains(recipe.id))
-                    .collect(Collectors.toList());
-
-            randomRecipeAdapter.setRecipes(filteredRecipes);
-            randomRecipeAdapter.notifyDataSetChanged();
+            randomRecipeAdapter = new RandomRecipeAdapter(BreakfastGenerateActivity.this,response.recipes,recipeClickListener);
+            recyclerView.setAdapter(randomRecipeAdapter);
         }
 
         @Override
         public void didError(String message) {
-            Toast.makeText(BreakfastGenerateActivity.this, message, Toast.LENGTH_SHORT).show();
+            Toast.makeText(BreakfastGenerateActivity.this, "message", Toast.LENGTH_SHORT).show();
         }
     };
 
-    private final AdapterView.OnItemSelectedListener spinnerSelectedListener = new AdapterView.OnItemSelectedListener() {
+    private final AdapterView.OnItemSelectedListener spinnerSelectedListener= new AdapterView.OnItemSelectedListener() {
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-            updateTagsList(breakfastTags, adapterView.getSelectedItem().toString());
+            breakfastTags.clear();
+            breakfastTagsExclude.clear();
+            breakfastTags.add("breakfast");
+            String selectedTag = adapterView.getSelectedItem().toString();
+            if (!selectedTag.equals("breakfast")) {
+                breakfastTags.add(selectedTag);
+            }
+
             Log.d("BreakfastGenerateActivity", "First tagsInc: " + breakfastTags.toString());
             Log.d("BreakfastGenerateActivity", "First tagsExclude: " + breakfastTagsExclude.toString());
-            manager.getRandomRecipes(randomRecipeResponseListener, breakfastTags, breakfastTagsExclude);
+
+            manager.getRandomRecipes(randomRecipeResponseListener,breakfastTags,breakfastTagsExclude);
             dialog.show();
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
+
         }
     };
 
-    private final AdapterView.OnItemSelectedListener spinnerSelectedListener2 = new AdapterView.OnItemSelectedListener() {
+    private final AdapterView.OnItemSelectedListener spinnerSelectedListener2= new AdapterView.OnItemSelectedListener() {
         @Override
         public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-            updateTagsList(breakfastTagsExclude, adapterView.getSelectedItem().toString());
+            breakfastTags.clear();
+            breakfastTags.add("breakfast");
+
+            breakfastTagsExclude.clear();
+            breakfastTagsExclude.add(adapterView.getSelectedItem().toString());
+
             Log.d("BreakfastGenerateActivity", "Second tagsInc: " + breakfastTags.toString());
             Log.d("BreakfastGenerateActivity", "Second tagsExclude: " + breakfastTagsExclude.toString());
-            manager.getRandomRecipes(randomRecipeResponseListener, breakfastTags, breakfastTagsExclude);
+            manager.getRandomRecipes(randomRecipeResponseListener,breakfastTags,breakfastTagsExclude);
             dialog.show();
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
+
         }
     };
 
-    private void updateTagsList(List<String> tagsList, String selectedTag) {
-        tagsList.clear();
-        tagsList.add("breakfast");
-        if (!selectedTag.equals("breakfast")) {
-            tagsList.add(selectedTag);
-        }
-    }
+
 
     private final RecipeClickListener recipeClickListener = new RecipeClickListener() {
         @Override
         public void onRecipeClick(String id) {
-            clickedRecipeIds.add(id);
-            for (Recipe recipe : randomRecipeAdapter.getList()) {
-                if (String.valueOf(recipe.id).equals(id)) {
-                    String title = recipe.title;
-                    Log.d("BreakfastGenerateActivity", "Added clicked recipe - ID: " + id + ", Title: " + title);
-                    break;
-                }
-            }
             startActivity(new Intent(BreakfastGenerateActivity.this, RecipeDetailsActivity.class)
-                    .putExtra("id", id));
+                    .putExtra("id",id));
         }
     };
 }
+
